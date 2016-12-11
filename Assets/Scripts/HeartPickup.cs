d5ca8f659203cd13328ada38998d325eb2ec951b
Issi <depiotr01@gmail.com>
2016-12-11 13:04:20 +0100

    HealthBar Pre touch screen commit

---
+++ Assets/Scripts/HeartPickup.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeartPickup : MonoBehaviour {
+
+    public int healthToGive;
+
+    public AudioSource collectSound;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.GetComponent<PlayerControler>() == null)
+            return;
+        HealthManager.HurtPlayer(-healthToGive);
+
+        collectSound.Play();
+
+        Destroy(gameObject);
+    }
+}
